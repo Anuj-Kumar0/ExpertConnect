a5692c0c55711e8e0ff0c64c8864467b84c4ae3c
Anuj Kumar <anujkumar21052004@gmail.com>
2026-05-10 10:32:12 +0530

    feat: setup server.js to start express app

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import http from "http";
+import { Server } from "socket.io";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+const server = http.createServer(app);
+
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
